1f49f308e59b6eba7b200522cc8964cf8f39bc78
PoBlue <250299430@qq.com>
2018-02-28 12:04:14 +0800

    [step 3] defined class: Rocket

---
+++ src/Rocket.java
@@ -0,0 +1,30 @@
+public class Rocket implements SpaceShip {
+    private int maxItemWeight;
+    private int weight;
+    private int itemsWeight = 0;
+
+    public Rocket(int maxItemWeight, int weight) {
+        this.maxItemWeight = maxItemWeight;
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean launch() {
+        return true;
+    }
+
+    @Override
+    public boolean land() {
+        return true;
+    }
+
+    @Override
+    public boolean canCarry(Item item) {
+        return ( this.weight + this.itemsWeight + item.getWeight() ) <= maxItemWeight;
+    }
+
+    @Override
+    public void carry(Item item) {
+        this.itemsWeight += item.getWeight();
+    }
+}
